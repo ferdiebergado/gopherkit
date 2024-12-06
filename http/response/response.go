eb1dc3fb06109b45c7a6f9079fd1c6cfb6b11911
ferdie bergado <ferdiebergado@gmail.com>
2024-12-06 22:06:51 +0800

    Added http/response package

---
+++ http/response/response.go
@@ -0,0 +1,82 @@
+package response
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+const (
+	templatesDir = "templates"
+	layoutFile   = "layout.html"
+)
+
+// Sends a JSON response
+func JSON[T any](w http.ResponseWriter, status int, v T) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return fmt.Errorf("encode json: %w", err)
+	}
+
+	return nil
+}
+
+// Sends an HTML response
+func HTML(w http.ResponseWriter, data any, templateFiles ...string) {
+	layoutTemplate := filepath.Join(templatesDir, layoutFile)
+	targetTemplates := []string{layoutTemplate}
+
+	targetTemplates = append(targetTemplates, templateFiles...)
+
+	funcMap := templateFuncs()
+
+	templates, err := template.New("template").Funcs(funcMap).ParseFiles(targetTemplates...)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var buf bytes.Buffer
+
+	if err := templates.ExecuteTemplate(&buf, layoutFile, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = buf.WriteTo(w)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// Creates a template funcmap
+func templateFuncs() template.FuncMap {
+	return template.FuncMap{
+		"attr": func(s string) template.HTMLAttr {
+			return template.HTMLAttr(s)
+		},
+		"safe": func(s string) template.HTML {
+			return template.HTML(s)
+		},
+		"url": func(s string) template.URL {
+			return template.URL(s)
+		},
+		"js": func(s string) template.JS {
+			return template.JS(s)
+		},
+		"jsstr": func(s string) template.JSStr {
+			return template.JSStr(s)
+		},
+		"css": func(s string) template.CSS {
+			return template.CSS(s)
+		},
+	}
+}
