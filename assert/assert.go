494c362b8892d0421ac9e5c7891913656f21965c
ferdie bergado <ferdiebergado@gmail.com>
2024-12-15 08:01:22 +0800

    Renamed functions to avoid stutter

--- assert/assert.go
+++ assert/assert.go
@@ -3,61 +3,62 @@ package assert
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 )
 
 // AssertEqual asserts that two values are equal.
-func AssertEqual(t *testing.T, expected, actual interface{}, msg ...string) {
+func Equal(t *testing.T, expected, actual interface{}, msg ...string) {
 	if !reflect.DeepEqual(expected, actual) {
 		message := formatMessage("expected", expected, actual, msg...)
-		t.Errorf(message)
+		t.Error(message)
 	}
 }
 
 // AssertNotEqual asserts that two values are not equal.
-func AssertNotEqual(t *testing.T, expected, actual interface{}, msg ...string) {
+func NotEqual(t *testing.T, expected, actual interface{}, msg ...string) {
 	if reflect.DeepEqual(expected, actual) {
 		message := formatMessage("not expected", expected, actual, msg...)
-		t.Errorf(message)
+		t.Error(message)
 	}
 }
 
 // AssertNoError asserts that an error is nil.
-func AssertNoError(t *testing.T, err error, msg ...string) {
+func NoError(t *testing.T, err error, msg ...string) {
 	if err != nil {
 		message := formatMessage("no error", nil, err, msg...)
-		t.Errorf(message)
+		t.Error(message)
 	}
 }
 
 // AssertError asserts that an error is not nil.
-func AssertError(t *testing.T, err error, msg ...string) {
+func Error(t *testing.T, err error, msg ...string) {
 	if err == nil {
 		message := formatMessage("error", "non-nil error", err, msg...)
-		t.Errorf(message)
+		t.Error(message)
 	}
 }
 
 // AssertContains asserts that a string contains a substring.
-func AssertContains(t *testing.T, s, substr string, msg ...string) {
+func Contains(t *testing.T, s, substr string, msg ...string) {
 	if !contains(s, substr) {
 		message := formatMessage(fmt.Sprintf("'%s' to contain", substr), substr, s, msg...)
-		t.Errorf(message)
+		t.Error(message)
 	}
 }
 
 // AssertLen asserts that a collection has the expected length.
-func AssertLen(t *testing.T, collection interface{}, length int, msg ...string) {
+func Len(t *testing.T, collection interface{}, length int, msg ...string) {
 	actualLen := reflect.ValueOf(collection).Len()
 	if actualLen != length {
 		message := formatMessage("length", length, actualLen, msg...)
-		t.Errorf(message)
+		t.Error(message)
 	}
 }
 
 // Helper function to check if a string contains a substring.
 func contains(s, substr string) bool {
-	return len(s) >= len(substr) && s[:len(substr)] == substr
+	return len(substr) == 0 || strings.Contains(s, substr)
 }
 
 // Helper function to format error messages.
