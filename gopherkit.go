3ee254f9bc318dbc5734b364fe0d32702819f416
ferdie bergado <ferdiebergado@gmail.com>
2024-12-26 08:51:36 +0000

    Added Sum()

--- gopherkit.go
+++ gopherkit.go
@@ -1,6 +1,9 @@
 package gopherkit
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 // Parse integer values with a default value
 func ParseInt(value string, defaultValue int) int {
@@ -11,4 +14,43 @@ func ParseInt(value string, defaultValue int) int {
 	}
 
 	return parsed
+}
+
+type Integer interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type UnsignedInt interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type Float interface {
+	~float32 | ~float64
+}
+
+type Complex interface {
+	~complex64 | ~complex128
+}
+
+type Number interface {
+	Integer | Float | Complex
+}
+
+// Calculate the sum of variadic or slice of numbers
+func Sum[T Number](values ...interface{}) T {
+	var total T
+
+	for _, val := range values {
+		switch v := val.(type) {
+		case []T: // If the input is a slice
+			for _, item := range v {
+				total += item
+			}
+		case T: // If the input is a single numeric value
+			total += v
+		default:
+			panic(fmt.Sprintf("Unsupported type: %v", v))
+		}
+	}
+	return total
 }
--- gopherkit_test.go
+++ gopherkit_test.go
@@ -45,10 +45,93 @@ func TestParseInt(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
 			result := ParseInt(tt.input, tt.defaultValue)
 			if result != tt.expected {
 				t.Errorf("ParseInt(%q, %d) = %d; expected %d", tt.input, tt.defaultValue, result, tt.expected)
 			}
 		})
 	}
+}
+
+type SumTestData[T Number] struct {
+	name     string
+	input    []T
+	expected T
+}
+
+func TestSum(t *testing.T) {
+	tests := []SumTestData[int]{
+		{
+			name:     "Sum of empty slice",
+			input:    []int{},
+			expected: 0,
+		},
+		{
+			name:     "Sum of slice of ints",
+			input:    []int{1, 2, 3},
+			expected: 6,
+		},
+		{
+			name:     "Sum of slices with negative ints",
+			input:    []int{5, -2, 5},
+			expected: 8,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			result := Sum[int](tt.input)
+			if result != tt.expected {
+				t.Errorf("Sum(%v)= %d; expected %d", tt.input, result, tt.expected)
+			}
+		})
+	}
+
+	float64test := []SumTestData[float64]{
+		{
+			name:     "Sum of empty float64 slice",
+			input:    []float64{},
+			expected: 0,
+		},
+		{
+			name:     "Sum of slice of float64",
+			input:    []float64{1.25, 2.5, 3.01},
+			expected: 6.76,
+		},
+	}
+
+	for _, tt := range float64test {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			result := Sum[float64](tt.input)
+			if result != tt.expected {
+				t.Errorf("Sum(%v)= %f; expected %f", tt.input, result, tt.expected)
+			}
+		})
+	}
+
+	t.Run("Sum of variadic ints", func(t *testing.T) {
+		t.Parallel()
+
+		result := Sum[int](1, 2, 3)
+		expected := 6
+		if result != expected {
+			t.Errorf("Sum(%v)= %d; expected %d", "1,2,3", result, expected)
+		}
+	})
+
+	t.Run("Sum of variadic float64", func(t *testing.T) {
+		t.Parallel()
+
+		result := Sum[float64](1.25, 2.5, 3.01)
+		expected := 6.76
+		if result != expected {
+			t.Errorf("Sum(%v)= %f; expected %f", "1.25, 2.5, 3.01", result, expected)
+		}
+	})
 }
