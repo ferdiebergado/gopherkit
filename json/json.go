e8239279f7a5632525dd96f70bf8dbf0b280cfd3
ferdie bergado <ferdiebergado@gmail.com>
2024-12-27 06:51:33 +0000

    Added documentation comments

--- json/json.go
+++ json/json.go
@@ -5,12 +5,14 @@ import (
 	"encoding/json"
 )
 
+// Decode json data to a struct
 func Decode[T any](data []byte, target *T) error {
 	decoder := json.NewDecoder(bytes.NewReader(data))
 	decoder.DisallowUnknownFields()
 	return decoder.Decode(target)
 }
 
+// Encode data into json
 func Encode(v any) ([]byte, error) {
 	return json.Marshal(v)
 }
